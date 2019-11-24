6ed8bf9330be80f5b83be9ab6b021e78e693864b
c5295360 <illia.halynets@sap.com>
2019-11-24 16:17:37 +0300

    change image, remove debuggers, add new information in forecast

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,6 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiaWx5YTI1IiwiYSI6ImNrMzVweWRlbjAwNDAzZGxpZHk0bXMycTcifQ.jg-Dkr6h9_Dv_WY4mY8pxw&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
-        debugger
         if (error) {
             callback('Unable to connect to location services!')
         } else if (!body.features || !body.features.length) {
